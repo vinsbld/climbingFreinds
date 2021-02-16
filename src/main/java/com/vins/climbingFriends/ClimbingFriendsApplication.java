54fe4f38fde07c9485c0b98944d4c1d4f046ee35
vinsbld <vincent.blndel@gmail.com>
2021-02-16 15:51:07 +0100

    first-commit

---
+++ src/main/java/com/vins/climbingFriends/ClimbingFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.vins.climbingFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vins/climbingFriends/ClimbingFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vins.climbingFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
