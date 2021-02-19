1a67df8dd5b4e2ea0d7a7b60e7cdf2786dabd979
vinsbld <vincent.blndel@gmail.com>
2021-02-19 18:12:42 +0100

    create saveAndRetreive integrationTest workinprogress

--- src/test/java/com/vins/climbingFriends/integrationTest/CommentaireIntegrationTest.java
+++ src/test/java/com/vins/climbingFriends/integrationTest/CommentaireIntegrationTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
 public class CommentaireIntegrationTest {
 
     @Autowired
