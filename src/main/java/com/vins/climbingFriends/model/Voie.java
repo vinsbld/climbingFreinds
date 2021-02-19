c28ff3e772d0cc7200c21a7e3443b9b989e894ea
vinsbld <vincent.blndel@gmail.com>
2021-02-19 10:38:10 +0100

    create siteEscalade longueur + voie

---
+++ src/main/java/com/vins/climbingFriends/model/Voie.java
@@ -0,0 +1,25 @@
+package com.vins.climbingFriends.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Voie implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int cotation;
+}
